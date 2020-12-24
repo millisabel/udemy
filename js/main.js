371c54bcdcc510ab01f4253a84462b94167c8aff
millisabel <38396309+millisabel@users.noreply.github.com>
2020-12-24 19:58:23 +0200

    practice, 2

--- js/main.js
+++ js/main.js
@@ -10,12 +10,29 @@ const personalMovieDB = {
     privat: false,
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?'),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?');
+let count = 0;
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    let a, b;
+
+    a = prompt('Один из последних просмотренных фильмов?', '');
+    b = prompt('На сколько оцените его?', '');
+
+    if (a !== null & b !== null & a !== '' & b !== '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+    } else{
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    alert("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+    alert("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+    alert("Вы киноман");
+} else {
+    alert("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
